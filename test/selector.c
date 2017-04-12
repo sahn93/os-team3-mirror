551cd7912d7b3487acc61e72720d9a5eba248470
craig1583 <ahnsc93@gmail.com>
2017-04-12 21:28:55 +0900

    selector implemented

---
+++ test/selector.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define __NR_rotlock_write 382
+#define __NR_rotunlock_write 385
+
+int main(int argc, char *argv[]) {
+    FILE *fout = fopen("integer", "w");
+    int value;
+    if (argc==2) {
+        printf("The argument supplied is %s\n", argv[1]);
+        value = atoi(argv[1]);
+    } else {
+        printf("One argument expected.\n");
+        return 0;
+    }
+    
+    while(1) {
+        syscall(__NR_rotlock_write, 90, 90);
+        printf("selector: %d\n", value);
+        value++;
+        fprintf(fout, "%d\n", value);
+        fclose(fout);
+        syscall(__NR_rotunlock_write, 90, 90);
+    }
+    return 0;
+}
