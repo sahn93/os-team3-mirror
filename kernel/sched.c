477a1e62a57abb09450abafbaf47fb342318d6e9
naong606 <naong606@gmail.com>
2017-05-04 14:04:04 +0900

    Add TODOs in system calls

--- kernel/sched.c
+++ kernel/sched.c
@@ -1,14 +1,35 @@
 #include <linux/unistd.h>
 #include <linux/slab.h>
 #include <linux/kernel.h>
+#include <linux/sched.h>
 #include <asm-generic/errno-base.h>
 
 asmlinkage int sys_sched_setweight(pid_t pid, int weight) {
-	/*TODO*/
+	if (pid == 0)
+		pid = current->pid;
+	
+	/* Error cases :
+	     - task with given pid doesn't exist
+	     - task's policy is not SCHED_WRR
+		 - caller is neither the administrator, nor the owner of task
+		 - the owner of task (not admin) tries to increase its weight
+		 - weight < 1 || weight > 20
+	*/
+
+	// set task's weight here 
+
 	return 0;
 }
 
 asmlinkage int sys_sched_getweight(pid_t pid) {
-	/*TODO*/
+	if (pid == 0)
+		pid = current->pid;
+
+	/* Error cases :
+	     - task with given pid doesn't exist
+	     - task's policy is not SCHED_WRR
+	*/
+
+	// return task's weight below
 	return 0;
 }
