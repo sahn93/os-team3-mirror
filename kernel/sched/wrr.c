ccbbaa61192ab25432176d2e11df9a9fdc716e3b
naong606 <naong606@gmail.com>
2017-05-07 21:48:00 +0900

    Implement put_prev_task, set_curr_task, select_task_rq, task_tick

--- kernel/sched/wrr.c
+++ kernel/sched/wrr.c
@@ -18,6 +18,27 @@ void init_wrr_rq(struct wrr_rq *wrr_rq) {
 	raw_spin_lock_init(&wrr_rq->wrr_runtime_lock);
 }
 
+static void update_curr_wrr(struct rq *rq) {
+	struct task_struct *curr = rq->curr;
+	u64 delta_exec;
+
+	if (curr->sched_class != &wrr_sched_class)
+		return;
+
+	delta_exec = rq->clock_task - curr->se.exec_start;
+	if (unlikely((s64)delta_exec <= 0))
+		return;
+
+	schedstat_set(curr->statistics.exec_max,
+			  max(delta_exec, curr->se.statistics.exec_max));
+
+	curr->se.sum_exec_runtime += delta_exec;
+	account_group_exec_runtime(curr, delta_exec);
+
+	curr->se.exec_start = rq->clock_task;
+	cpuacct_charge(curr, delta_exec);
+}
+
 static void enqueue_task_wrr(struct rq *rq, struct task_struct *p, int flags) {
 	/*TODO*/
 }
@@ -32,17 +53,56 @@ static struct task_struct *pick_next_task_wrr(struct rq *rq) {
 }
 
 static void put_prev_task_wrr(struct rq *rq, struct task_struct *p) {
+	update_curr_wrr(rq);
 }
+
+	/*TODO*/
+#ifdef CONFIG_SMP
+static int select_task_rq_wrr(struct task_struct *p, int sd_flag, int flags) {
+	int cpu = task_cpu(p);
+	struct rq *rq;
+	struct wrr_rq *wrr_rq;
+	int min_wsum = 1987654321, min_cpu = -1, i;
+
+	if (p->nr_cpus_allowed == 1)
+		return cpu;
+
+	if (sd_flag & SD_BALANCE_WAKE)
+		return cpu;
+
+	rcu_read_lock();
+	for_each_cpu(i, tsk_cpus_allowed(p)) {
+		rq = cpu_rq(i);
+		wrr_rq = &rq->wrr;
+		if (wrr_rq->wrr_total_weight < min_wsum) {
+			min_wsum = wrr_rq->wrr_total_weight;
+			min_cpu = i;
+		}
+	}
+	rcu_read_unlock();
+	return min_cpu;
+}
+#endif
 
 static void set_curr_task_wrr(struct rq *rq) {
+	struct task_struct *p = rq->curr;
+	p->se.exec_start = rq->clock_task;
+	return;
 }
 
-static void task_tick_wrr(struct rq *rq, struct task_struct *curr, int queued) {
-	/*TODO*/
-}
+static void task_tick_wrr(struct rq *rq, struct task_struct *p, int queued) {
+	struct sched_wrr_entity *wrr_se = &p->wrr;
+	struct wrr_rq *wrr_rq = &rq->wrr;
 
-static void task_fork_wrr(struct task_struct *p) {
-	/*TODO*/
+	update_curr_wrr(rq);
+
+	if (--p->wrr.time_left)
+		return;
+
+	p->wrr.time_left = p->wrr.time_slice;
+
+	list_move_tail(&wrr_se->run_list, &wrr_rq->rq);
+	set_tsk_need_resched(p);
 }
 
 const struct sched_class wrr_sched_class = {
@@ -54,9 +114,9 @@ const struct sched_class wrr_sched_class = {
 	.put_prev_task		= put_prev_task_wrr,
 
 #ifdef CONFIG_SMP
+	.select_task_rq		= select_task_rq_wrr,
 #endif
 	
 	.set_curr_task		= set_curr_task_wrr,
 	.task_tick			= task_tick_wrr,
-	.task_fork			= task_fork_wrr
 };
