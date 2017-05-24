bd24db412c470227e8197aa21b5f2d0a46cc85bc
craig1583 <ahnsc93@gmail.com>
2017-05-24 18:49:40 +0900

    corrected missed boundary condition

--- kernel/gps.c
+++ kernel/gps.c
@@ -1,11 +1,6 @@
 #include <linux/gps.h>
 #include <linux/uaccess.h>
 #include <linux/gpscommon.h>
-#define FREETURN \
-        do { \
-            kfree(kbuf);\
-            return -EINVAL;\
-        } while(0)
 
 struct gps_location gpsloc;
 asmlinkage int sys_set_gps_location(struct gps_location __user *loc) {
@@ -24,20 +19,18 @@ asmlinkage int sys_set_gps_location(struct gps_location __user *loc) {
         return -EFAULT;
     }
 
-    if (kbuf->lat_fractional < 0 || kbuf->lat_fractional >= 1000000)
-        FREETURN;
-
-    if (kbuf->lng_fractional < 0 || kbuf->lng_fractional >= 1000000)
-        FREETURN;
-
-    if (kbuf->lat_integer > 90 || kbuf->lat_integer < -90)
-        FREETURN;
-
-    if (kbuf->lng_integer > 180 || kbuf->lng_integer < -180)
-        FREETURN;
-
-    if (kbuf->accuracy < 0)
-        FREETURN; 
+    if (
+            (kbuf->lat_fractional < 0 || kbuf->lat_fractional >= 1000000) ||
+            (kbuf->lng_fractional < 0 || kbuf->lng_fractional >= 1000000) ||
+            (kbuf->lat_integer > 90 || kbuf->lat_integer < -90) ||
+            (kbuf->lng_integer > 180 || kbuf->lng_integer < -180) ||
+            (kbuf->lat_integer == 90 && kbuf->lat_fractional !=0) ||
+            (kbuf->lng_integer == 180 && kbuf->lng_fractional !=0) ||
+            (kbuf->accuracy < 0)
+       ) {
+        kfree(kbuf);
+        return -EINVAL;
+    }
 
     spin_lock(&gps_lock);
         gpsloc.lat_integer = kbuf->lat_integer;
